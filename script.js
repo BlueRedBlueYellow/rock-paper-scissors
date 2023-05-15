6949efa815a845706dbb8bd16027502a9087071e
BlueRedBlueYellow <74393018+BlueRedBlueYellow@users.noreply.github.com>
2023-05-15 01:21:41 -0500

    Calculate and show scores

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const CHOICES = ["rock", "paper", "scissors"];
-
 const allButtons = document.querySelectorAll(".choice");
-const resultText = document.querySelector("#results");
+const resultText = document.querySelector("#round-result");
+const playerScore = document.querySelector("#player-score .score");
+const CPUScore = document.querySelector("#cpu-score .score");
+
+
 allButtons.forEach(button => button.addEventListener('click', playRound));
 
 let playerWins = 0;
-let computerWins = 0;
+let CPUwins = 0;
 let roundResults = ``;
 let currentRound = 0;
 
@@ -43,8 +46,12 @@ function calculateGameResults(roundResult) {
     if (roundResult == 'tie') {
         resultText.textContent = "Tie! Please pick again."; 
     } else if (roundResult) {
+        playerWins += 1 
+        playerScore.textContent = playerWins;
         resultText.textContent = "You won! Please pick again."; 
     } else {
+        CPUwins += 1;
+        CPUScore.textContent = CPUwins;
         resultText.textContent = "You lost. Please pick again."; 
     }
 
